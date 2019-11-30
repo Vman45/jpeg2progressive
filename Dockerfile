5de6c1575c1ea281f973c416059fc523fa741eb4
prytoegrian <prytoegrian@protonmail.com>
2019-11-30 20:18:35 +0100

    Start converting

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:3.9
 
-RUN apk add libjpeg-turbo-utils bash
+RUN apk add libjpeg-turbo-utils bash imagemagick && \
+    mkdir -p /tmp/images/results/convert && \
+    mkdir -p /tmp/images/results/jpegtran
 
 WORKDIR /tmp/images
--- convert.sh
+++ convert.sh
@@ -3,18 +3,21 @@
 for img in `find . -name "*" | egrep "\.jpe?g$"`
 do
     echo "$img"
-    #idout=`identify -verbose $img | grep -i interlace | grep -i none$`
+    name=`echo "$img" | egrep "[0-9]+\.jpe?g$" | egrep -o '[0-9]+'`
+    echo "$name"
+    idout=`identify -verbose $img | grep -i interlace | grep -i none$`
 
-    #if [[ -z $idout ]]
-    #then
-    #echo "-------------------------"
-    #echo "$img is progressive"
+    if [[ -z $idout ]]
+    then
+        echo "$img is progressive"
     #echo "....making copy of original with .prog extension"
     #/bin/cp -f $img $img.prog
     #echo "....converting to baseline"
     #convert $img -interlace none $img 
     #echo "....done!"
-    ##else
-    ##echo "$img is non-progressive"
-    #fi
+    else
+        echo "$img is non-progressive"
+        echo "... converting to progressive"
+        # convert "$img" -interlace Line "./results/convert/$img"
+    fi
 done
